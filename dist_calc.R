63e822fcbe04cc320bd7173a22259f8351e5332a
Kazım Anıl Eren <33596334+kazimanil@users.noreply.github.com>
2018-08-16 17:26:13 +0300

    Calculates distance between two locations.
    Uses latitude and longitude information

---
+++ dist_calc.R
@@ -0,0 +1,17 @@
+# Distance Calculator based on Latitude & Longitude of two locations.
+# Uses Haversine formula at https://www.movable-type.co.uk/scripts/latlong.html as reference
+
+dist_calc = function(lat1, len1, lat2, len2){
+  # radian transformation
+  lat1 = lat1 * pi / 180
+  lat2 = lat2 * pi / 180
+  len1 = len1 * pi / 180
+  len2 = len2 * pi / 180
+  # distances are calculated for sine functions.
+  dist_lat = (lat2 - lat1) / 2
+  dist_len = (len2 - len1) / 2
+  a = sin(dist_lat)^2 + (cos(lat1) * cos(lat2) * sin(dist_len)^2)
+  c = 2 * atan2(sqrt(a), sqrt(1-a))
+  d = 6371 * c
+  d
+}
