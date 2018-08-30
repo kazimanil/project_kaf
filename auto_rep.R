7bfec9b9bc16f7fad15c4a1124f3bd94c89ba117
kazimanil <kazimanileren@gmail.com>
2018-08-30 15:10:17 +0300

    Bug Fixes

--- auto_rep.R
+++ auto_rep.R
@@ -3,15 +3,14 @@
 ## Target    : Automatically replaces values in many columns.
 ## Function  :
 
-auto_rep = function(dt, pattern = NA, colnames = NA, old_val, new_val)
+auto_rep = function(dt, pattern = NA, cols = NA, old_val, new_val){
 	require("data.table")
-	{
-	if(is.na(colnames) & is.na(pattern)){
+	if(is.na(cols) & is.na(pattern)){
 		stop() # either columns or pattern of columns must be specified
 	}
-	if(is.na(colnames)){
-		colnames = colnames(dt)[grepl(pattern = pattern, colnames(dt))]		
+	if(is.na(cols)){
+		cols = colnames(dt)[grepl(pattern = pattern, colnames(dt))]		
 	} # if pattern is supplied, columns are chosen from data.table.
-	new_vals = as.data.table(sapply(dt[, colnames, with = FALSE], function(x){ifelse(x == old_val, new_val, x)}))
-	dt[, (colnames) := new_vals]
+	new_vals = as.data.table(sapply(dt[, cols, with = FALSE], function(x){ifelse(x == old_val, new_val, x)}))
+	dt[, (cols) := new_vals]
 	}
