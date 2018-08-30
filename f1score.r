795e964068cb71cf1b1b4d0ebbb5fc7d6ddf8c8b
kazimanil <kazimanileren@gmail.com>
2018-08-30 15:04:35 +0300

    Bug Fixes in F1 Scoring

--- f1score.r
+++ f1score.r
@@ -2,7 +2,7 @@ f1score = function(precision, recall){
     2 * precision * recall / (precision + recall)
 }
 
-f1_score2 = funcion(true_pos, false_pos, false_neg){
+f1_score2 = function(true_pos, false_pos, false_neg){
 	precision = true_pos / (true_pos + false_pos)
 	recall    = true_pos / (true_pos + false_neg)
 	2 * precision * recall / (precision + recall)
