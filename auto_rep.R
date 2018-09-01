284d3bc5fbe1b50f9c47b90fcad3b01d9714390b
kazimanil <kazimanileren@gmail.com>
2018-09-01 14:44:58 +0300

    Performance Improvements

--- auto_rep.R
+++ auto_rep.R
@@ -1,8 +1,8 @@
 ## Created By: Kazım Anıl Eren
 ## Created On: 2018-08-30
-## Edited  On: 2018-08-31
+## Edited  On: 2018-09-01
 ## Target    : Automatically replaces values in many columns.
-## Edits     : Fixed various bugs and expanded capacity
+## Edits     : Fixed various bugs, expanded capacity, improved speed.
 ## Function  :
 
 auto_rep = function(dt, pattern = NA, cols = NA, old_val, new_val){
@@ -13,8 +13,10 @@ auto_rep = function(dt, pattern = NA, cols = NA, old_val, new_val){
 	if(sum(is.na(cols)) > 0 ){
 		cols = colnames(dt)[grepl(pattern = pattern, colnames(dt))]		
 	} # if pattern is supplied, columns are chosen from data.table.
-	new_vals = as.data.table(sapply(dt[, cols, with = FALSE], 
-	                         Vectorize(function(x){ifelse(ifelse(is.na(old_val), is.na(x), x == old_val), new_val, x)})
-									))
+	if(is.na(old_val)){
+	new_vals = as.data.table(sapply(dt[, cols, with = FALSE], function(x){ifelse(is.na(x), new_val, x)}))
+	} else {
+	new_vals = as.data.table(sapply(dt[, cols, with = FALSE], function(x){ifelse(x == old_val, new_val, x)}))
+	}
 	dt[, (cols) := new_vals]
 	}
