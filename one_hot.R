c3e4b1bffb5072b01ae09ca03d88488cbcb395cc
kazimanil <kazimanileren@gmail.com>
2018-09-03 15:51:34 +0300

    Added separator option.

--- one_hot.R
+++ one_hot.R
@@ -1,9 +1,11 @@
 ## Created By: Kazım Anıl Eren
 ## Created On: 2018-08-31
+## Edited  On: 2018-09-03
 ## Target    : Automatically encodes character / factor columns into dummy variables.
+## Edits     : Separator for encoding is included in the function.
 ## Function  :
 
-one_hot = function(dt, cols){
+one_hot = function(dt, cols, sep = "_"){
   if(exists("cols") == FALSE){
     stop() # if no columns were given then function fails
   }
@@ -15,7 +17,7 @@ one_hot = function(dt, cols){
    uniquevals = `colnames<-`(uniquevals, "V1")
     for(k in 1:nrow(uniquevals)){
       newcol = as.character(uniquevals[k, 1])
-      dt[, (paste0(colnames(dt[, j, with = FALSE]), "_", newcol)) := 
+      dt[, (paste0(colnames(dt[, j, with = FALSE]), sep, newcol)) := 
             ifelse(is.na(get(colnames(dt[, j, with = FALSE]))), 0, ifelse(get(colnames(dt[, j, with = FALSE])) == uniquevals[k]$V1, 1, 0))]   
     }
   }
