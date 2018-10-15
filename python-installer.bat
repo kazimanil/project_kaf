7f5d2598b4d55f1e68013bc9a11d0ff2aee1b556
kazimanil <kazimanileren@gmail.com>
2018-10-15 09:29:38 +0300

    Python Installer for Plain Py

---
+++ python-installer.bat
@@ -0,0 +1,14 @@
+pip install --upgrade pip
+pip install pandas
+pip install numpy
+pip install jupyter
+pip install jupyterlab
+pip install Matplotlib
+pip install seaborn
+pip install Bokeh
+pip install plotly
+pip install SciKit-Learn
+pip install keras
+pip install Theano
+pip install Statsmodels
+pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.12.0-py3-none-any.whl
